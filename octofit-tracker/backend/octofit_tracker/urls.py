90951b0870375553db0a3b71b9c576eb583ade2d
MSS1104 <ramesh.chanda@landg.com>
2026-03-20 11:30:20 +0000

    Fix NameError in urls.py by importing os before use

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -13,16 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path, include
+codespace_name = os.environ.get('CODESPACE_NAME')
+
 from .views import api_root, router
 import os
 
-codespace_name = os.environ.get('CODESPACE_NAME')
-if codespace_name:
-    base_url = f"https://{codespace_name}-8000.app.github.dev"
-else:
-    base_url = "http://localhost:8000"
+def get_base_url(request):
+    codespace_name = os.environ.get('CODESPACE_NAME')
+    if codespace_name:
+        return f"https://{codespace_name}-8000.app.github.dev"
+    return "http://localhost:8000"
 
 urlpatterns = [
     path('', api_root, name='api-root'),
